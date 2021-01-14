c267a998317a4e9623525ec63dccf610d542c359
Roohi Singh <singhroohi184@gmail.com>
2021-01-14 00:52:04 -0500

    Initial commit

---
+++ src/main/kotlin/myFirst.kt
@@ -0,0 +1,18 @@
+
+fun main(args:Array<String>){
+    var name: String
+    name = "Kevin"
+    println(name)
+
+    var age:Int = 10
+    var  isAlive:Boolean=true
+    var marks:Float=97.4F
+    var percentage:Double=90.78
+    var gender:Char='M'
+
+    println(age)
+    println(isAlive)
+    println(marks)
+    println(percentage)
+    println(gender)
+}
